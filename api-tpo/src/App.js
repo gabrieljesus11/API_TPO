ada195621cd492383e4c2e2a06b959fdfd49838c
Agus <aguscafiero22@gmail.com>
2023-08-31 21:15:37 -0300

    intento de navbar

--- api-tpo/src/App.js
+++ api-tpo/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <Header />
+    <Header />,
+    <Navbar />
   );
 }
 
