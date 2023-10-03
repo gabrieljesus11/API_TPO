03b93e46d54e68adbb2888e2f431242797608012
gabrieljesus11 <56008696+gabrieljesus11@users.noreply.github.com>
2023-10-03 16:46:42 -0300

    Login Funcionando

--- api-tpo/src/components/Login/Login.js
+++ api-tpo/src/components/Login/Login.js
@@ -3,74 +3,112 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import './Login.css'
 import { CardContent } from '@mui/material';
+import { useState } from 'react';
+import loginMock from './loginMock.json'
+import { BrowserRouter as Router, Switch, 
+  Route, Redirect,} from "react-router-dom";
 
 export default function FormPropsTextFields() {
-  return (
-    <Box 
-      className="login-container"
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '45ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
+
+const [userName, setUserName] = useState('')
+const [password, setPassword] = useState('')
+const [errorMessages, setErrorMessages] = useState({});
+const [isSubmitted, setIsSubmitted] = useState(false);
+
+const handleSubmit = (event) =>{
+  event.preventDefault();
+
+  const userData = loginMock.database.find((user) => user.usuario === userName);
+  
+  if(userData){
+    if(userData.password !== password){
+      setErrorMessages({"name" : "password", message: loginMock.errors.password})
+    }
+    else{
+      setIsSubmitted(true)
+      window.location.href = './'
+    }
+  }
+  else{
+    setErrorMessages({name: "username", message: loginMock.errors.username})
+  }
 
 
+}
 
+// Generate JSX code for error message
+const renderErrorMessage = (name) =>{
+  console.log(name, name === errorMessages.name)
+  if(name === errorMessages.name){ 
+    return (<div className="error">{errorMessages.message}</div>);}
+}
 
-    <div >
-
+const renderForm = (
+  <div >
     <div id="login-container">
-        <TextField
-          required
-          id="outlined-required"
-          label="Correo electrónico"
-          defaultValue=""
-        />
+      <TextField
+        required
+        id="outlined-required"
+        label="Correo electrónico"
+        defaultValue=""
+        value={userName}
+        onChange={(event) => {
+          setUserName(event.target.value);
+        }}
+      />
 
-        <TextField
-          required
-          id="outlined-required"
-          label="Contraseña"
-          defaultValue=""
-        />
+      <TextField
+        required
+        type='password'
+        id="outlined-required"
+        label="Contraseña"
+        defaultValue=""
+        value={password}
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      {renderErrorMessage(errorMessages.name)}
 
-        <div className='olvideContrasena'>
-          Olvidé mi contraseña
-        </div>
+      <div className='olvideContrasena'>
+        Olvidé mi contraseña
       </div>
-        
-
-<CardActions>
-<div className='registro'>
-          ¿No eres miembro?
-        </div>
-        <div className='registro2'>
-          <Link to={"/Registro"} className="registro2">Registrate acá</Link>
-        </div>
-</CardActions>
-
-
-
-    <CardActions id='botones-login'>
-
-      <div className="botonSecundario">
-        <Link to={"/"} className="linkSecundario">Volver</Link>
-      </div>
-
-      <div className="botonPrincipal">
-        <Link to={"/miPerfil"} className="linkPrimario">Ingresar</Link>
-      </div>
-      
-    </CardActions>
-
-
     </div>
 
-    </Box>
-  );
+    <CardActions>
+    <div className='registro'>
+      ¿No eres miembro?
+    </div>
+    <div className='registro2'>
+      <Link to={"/Registro"} className="registro2">Registrate acá</Link>
+    </div>
+    </CardActions>
+
+    <CardActions id='botones-login'>
+    <div className="botonSecundario">
+      <Link to={"/"} className="linkSecundario">Volver</Link>
+    </div>
+    <div className="botonPrincipal">
+      <Link className="linkPrimario" onClick={handleSubmit}>Ingresar</Link>
+    </div>
+    </CardActions>
+  </div>
+);
+
+return (
+  <Box 
+    className="login-container"
+    component="form"
+    sx={{
+      '& .MuiTextField-root': { m: 1, width: '45ch' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    {renderForm}
+  </Box>
+);
 }
