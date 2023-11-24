21f64bc0722373cd9cea51225c375bff7d9c6534
Agus <aguscafiero22@gmail.com>
2023-11-24 20:56:20 -0300

    actualizo2

--- api-tpo/src/components/PDP/PDP.js
+++ api-tpo/src/components/PDP/PDP.js
@@ -1,11 +1,7 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ReviewsIcon from '@mui/icons-material/Reviews';
-import { ListItem, ListItemButton, colors } from '@mui/material';
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import PersonIcon from '@mui/icons-material/Person';
 import Chip from '@mui/material/Chip';
@@ -14,7 +10,6 @@ import { Link } from "react-router-dom";
 import foto from "../../img/imagenPDP.png";
 import './PDP.css';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
 
--- api-tpo/src/components/SideMenu/SideMenu.js
+++ api-tpo/src/components/SideMenu/SideMenu.js
@@ -150,7 +150,7 @@ export default function TemporaryDrawer() {
               Cursos Disponibles
             </ListItemButton>
           </ListItem>
-          <ListItem disablePadding>
+          <ListItem disablePadding sx={{display: UserProvider.user != null ? 'contents' : 'none'}}>
             <ListItemButton to={"/abmcursos"}>
               <ListItemIcon>
                 <SchoolIcon />
